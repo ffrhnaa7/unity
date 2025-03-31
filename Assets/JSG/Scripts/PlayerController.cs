1881bc12d95df353da9e5c33f020c08aa3f370c5
JSG0626 <tmdrjs1419@naver.com>
2025-03-31 22:32:42 +0900

    <Fix> 충돌하는 PlayerController Stash 정리

--- Assets/JSG/Scripts/PlayerController.cs
+++ Assets/JSG/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿ using UnityEngine;
+﻿using UnityEngine;
 #if ENABLE_INPUT_SYSTEM 
 using UnityEngine.InputSystem;
 #endif
@@ -91,8 +91,7 @@ namespace StarterAssets
         private float _verticalVelocity;
         private float _terminalVelocity = 53.0f;
         private bool _armed = false;
-        private float _hp;
-
+        private float _hp = 0.0f;
         // timeout deltatime
         private float _jumpTimeoutDelta;
         private float _fallTimeoutDelta;
@@ -142,7 +141,7 @@ namespace StarterAssets
         private void Start()
         {
             _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
-            
+
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<StarterAssetsInputs>();
@@ -159,7 +158,7 @@ namespace StarterAssets
             _jumpTimeoutDelta = JumpTimeout;
             _fallTimeoutDelta = FallTimeout;
 
-            //
+            // 
             _hp = MaxHP;
         }
 
