aac1512f859b8c29efe87ce8f721fd9ea934c07c
gojob123 <122528455+gojob123@users.noreply.github.com>
2025-04-01 13:54:52 +0900

    Chasing Player
    Trigger가 발동되면 등장 애니메이션이 나오고 등장 애니메이션이 끝나면 플레이어를 따라오는 애니메이션까지 완료

--- Assets/SHD/Scripts/BossAI.cs
+++ Assets/SHD/Scripts/BossAI.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossAI : MonoBehaviour
 {
+    public Transform player;
+    public Animator animator;
+    public NavMeshAgent agent;
+
+    public float attackRange = 2.0f;
+
+    private bool isAttacking = false;
+    private bool isActive = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,30 @@ public class BossAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isActive) return; // 등장 애니메이션이 끝날때까지 대기
+
+        ChasePlayer();
+    }
+
+    // Chasing 함수
+    void ChasePlayer()
+    {
+        if (isAttacking) // 공격 중이면 Chase하지 않음
+            return;
+
+        agent.SetDestination(player.position);
+
+        // 플레이어 방향을 바라보는 코드
+        Vector3 targetPos = player.position;
+        targetPos.y = transform.position.y;
+        agent.transform.LookAt(targetPos);
+
+        animator.SetBool("isChasing", true);
+    }
+
+    // 보스 등장 애니메이션이 끝났을 때
+    public void ActivateAI()
+    {
+        isActive = true;
     }
 }
